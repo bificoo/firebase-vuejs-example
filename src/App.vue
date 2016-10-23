56be67caa085b9feb529f1f7035624335e5f87ec
Bificoo <bificoo@gmail.com>
2016-10-23 11:35:03 +0800

    Add firebase config

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,28 @@
 <template>
   <div id="app">
-    <h1>{{ msg }}</h1>
+    <h1>Firebase + Vue.js example</h1>
   </div>
 </template>
 
 <script lang="coffee" type="text/coffeescript">
+  firebase = require 'firebase'
+
+  config = {
+    apiKey: "AIzaSyBA8CnXKlkk2seKlj6KIAVJrxu9moNmm_g"
+    authDomain: "jsdc2016-54e08.firebaseapp.com"
+    databaseURL: "https://jsdc2016-54e08.firebaseio.com"
+    storageBucket: "jsdc2016-54e08.appspot.com"
+    messagingSenderId: "12243581682"
+  }
+  firebase.initializeApp(config)
+
   module.exports =
     data: () ->
       return {
-        msg: 'Hello Vue!'
+        newUser: {
+          name: ''
+          email: ''
+        }
       }
 </script>
 
